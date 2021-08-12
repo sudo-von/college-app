4ab8f53a936af610cc88d57079f7c1ccef6b8295
VoN <sudo.von.contact@gmail.com>
2021-08-12 15:39:24 -0500

    feat: :sparkles: Add University entity

--- go/entity/university.go
+++ go/entity/university.go
@@ -1,19 +1,13 @@
 package entity
 
-const (
-	FacultadSistemas = "60a1b132cc1fa10008fa31ad"
-)
-
 type University struct {
-	ID          string
-	Name        string
-	Description string
-	Picture     string
+	ID             string
+	Name           string
+	ProfilePicture string
 }
 
 type UniversityPayload struct {
-	ID          string
-	Name        string
-	Description string
-	Picture     string
+	ID             string
+	Name           string
+	ProfilePicture string
 }
