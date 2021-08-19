b162609e0f8395827b69a3f65e82f5920adb9886
VoN <sudo.von.contact@gmail.com>
2021-08-19 17:35:18 -0500

    feat: :sparkles: Add BasicAuth middleware

---
+++ go/api/middleware/jwt.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"context"
+	"freelancer/college-app/go/usecase/jwt"
+	"net/http"
+)
+
+type ContextKey struct {
+	Name string
+}
+
+func (k *ContextKey) String() string {
+	return "studyapp context value " + k.Name
+}
+
+var (
+	ContextKeyUser = &ContextKey{"User"}
+)
+
+func BasicAuth(uses *jwt.Service) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			email, password, ok := r.BasicAuth()
+			if ok {
+				user, err := uses.AuthenticateUser(email, password)
+				if err != nil {
+					if err.Error() == "not found" {
+						http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+						return
+					}
+					http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+					return
+				}
+				ctx := context.WithValue(r.Context(), ContextKeyUser, user)
+				next.ServeHTTP(w, r.WithContext(ctx))
+				return
+			}
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		})
+	}
+}
