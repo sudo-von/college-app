843dbdcb79642e7e766f28ec5aa49c5a984c9d3a
VoN <sudo.von.contact@gmail.com>
2021-08-21 03:30:27 -0500

    feat: :sparkles: Finish CreateContact use case

--- go/usecase/contact/service.go
+++ go/usecase/contact/service.go
@@ -20,7 +20,7 @@ func (s *Service) CreateContact(newContact entity.ContactPayload) error {
 
 	// Regex to verify if there are only numbers.
 	isNumerical := regexp.MustCompile(`^[0-9]*$`)
-	// Verify if the contact number is valid
+	// Verifies if the contact number is valid
 	validContactNumber := false
 	if isNumerical.MatchString(newContact.ContactNumber) && len(newContact.ContactNumber) == 12 {
 		validContactNumber = true
@@ -28,6 +28,15 @@ func (s *Service) CreateContact(newContact entity.ContactPayload) error {
 	if !validContactNumber {
 		return errors.New("invalid contact number")
 	}
-
+	// Verifies if the given user_id has registered his contact yet.
+	_, err := s.contactRepository.GetContactByUserID(newContact.UserID)
+	if err == nil {
+		return errors.New("given user_id already has a contact registered")
+	}
+	// Stores new contact.
+	err = s.contactRepository.CreateContact(newContact)
+	if err != nil {
+		return err
+	}
 	return nil
 }
