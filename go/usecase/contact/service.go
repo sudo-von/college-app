cd61571a5d65eba0dba27f48e01c44ae38731883
VoN <sudo.von.contact@gmail.com>
2021-08-21 04:13:31 -0500

    feat: :sparkles: Add UpdateContactByUserID use case

--- go/usecase/contact/service.go
+++ go/usecase/contact/service.go
@@ -28,7 +28,7 @@ func (s *Service) CreateContact(newContact entity.ContactPayload) error {
 
 	// Regex to verify if there are only numbers.
 	isNumerical := regexp.MustCompile(`^[0-9]*$`)
-	// Verifies if the contact number is valid
+	// Verifies if the contact number is valid.
 	validContactNumber := false
 	if isNumerical.MatchString(newContact.ContactNumber) && len(newContact.ContactNumber) == 12 {
 		validContactNumber = true
@@ -46,5 +46,42 @@ func (s *Service) CreateContact(newContact entity.ContactPayload) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (s *Service) UpdateContactByUserID(userID string, newContact entity.UpdateContactPayload) error {
+
+	// Gets old contact.
+	oldContact, err := s.contactRepository.GetContactByUserID(userID)
+	if err != nil {
+		return err
+	}
+	// If both phone numbers are different, checks if the new contact number is valid.
+	if oldContact.ContactNumber != newContact.ContactNumber {
+		// Regex to verify if there are only numbers.
+		isNumerical := regexp.MustCompile(`^[0-9]*$`)
+		// Verifies if the contact number is valid.
+		validContactNumber := false
+		if isNumerical.MatchString(newContact.ContactNumber) && len(newContact.ContactNumber) == 12 {
+			validContactNumber = true
+		}
+		if !validContactNumber {
+			return errors.New("invalid contact number")
+		}
+	}
+	// Creates a new user payload and then replaces the old one.
+	updatedContact := entity.ContactPayload{
+		ID:            oldContact.ID,
+		UserID:        oldContact.UserID,
+		ContactName:   newContact.ContactName,
+		ContactNumber: newContact.ContactNumber,
+		Message:       newContact.Message,
+		CreationDate:  oldContact.CreationDate,
+	}
+	err = s.contactRepository.UpdateContactByUserID(updatedContact)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
