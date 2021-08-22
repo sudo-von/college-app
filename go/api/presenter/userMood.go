715b931042aa0b2b3c87187d5d13fb7b55df7ff5
VoN <sudo.von.contact@gmail.com>
2021-08-22 01:02:18 -0500

    feat: :sparkles: Add Render method for the UserMoodResponse struct

--- go/api/presenter/userMood.go
+++ go/api/presenter/userMood.go
@@ -1,8 +1,14 @@
 package presenter
 
+import "net/http"
+
 type UserMoodResponse struct {
 	ID           string `json:"id"`
 	UserID       string `json:"user_id"`
 	Mood         int    `json:"mood"`
 	CreationDate string `json:"creation_date"`
+}
+
+func (ur *UserMoodResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
