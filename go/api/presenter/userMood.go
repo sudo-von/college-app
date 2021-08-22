153ca6272058768c6507a7ba7dfecad8bc80e3b6
VoN <sudo.von.contact@gmail.com>
2021-08-22 01:02:54 -0500

    feat: :sparkles: Add function to pass a user mood entity struct to a user mood response struct

--- go/api/presenter/userMood.go
+++ go/api/presenter/userMood.go
@@ -1,6 +1,9 @@
 package presenter
 
-import "net/http"
+import (
+	"freelancer/college-app/go/entity"
+	"net/http"
+)
 
 type UserMoodResponse struct {
 	ID           string `json:"id"`
@@ -11,4 +14,13 @@ type UserMoodResponse struct {
 
 func (ur *UserMoodResponse) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+func ToUserMoodPresenter(um entity.UserMood) UserMoodResponse {
+	return UserMoodResponse{
+		ID:           um.ID,
+		UserID:       um.UserID,
+		Mood:         um.Mood,
+		CreationDate: um.CreationDate.UTC().Format("2006-01-02"),
+	}
 }
