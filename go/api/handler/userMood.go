10195d271886157ef35226f7036555c7af6d6851
VoN <sudo.von.contact@gmail.com>
2021-08-23 14:33:32 -0500

    feat: :sparkles: Add user mood controller

---
+++ go/api/handler/userMood.go
@@ -0,0 +1,58 @@
+package handler
+
+import (
+	"errors"
+	"net/http"
+
+	"freelancer/college-app/go/api/middleware"
+	"freelancer/college-app/go/api/presenter"
+	"freelancer/college-app/go/entity"
+	"freelancer/college-app/go/token"
+	"freelancer/college-app/go/usecase/user_mood"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+)
+
+type UserMoodController struct {
+	UserMoodService user_mood.Service
+	TokenService    token.Service
+}
+
+func NewUserMoodController(userMood user_mood.Service, token token.Service) *UserMoodController {
+	return &UserMoodController{
+		UserMoodService: userMood,
+		TokenService:    token,
+	}
+}
+
+func (c *UserMoodController) Routes() chi.Router {
+	r := chi.NewRouter()
+	r.Use(middleware.IsAuthorized(c.TokenService))
+	r.Get("/", c.Show)
+	return r
+}
+
+// Show returns a user mood given the user id from the context.
+func (c *UserMoodController) Show(w http.ResponseWriter, r *http.Request) {
+
+	userID, ok := r.Context().Value(middleware.ContextKeyUserID).(string)
+	if !ok {
+		CheckError(errors.New("user not in context"), w, r)
+		return
+	}
+
+	userMoodFilters := entity.UserMoodFilters{
+		CreationDate: ParamToDate("creation_date", r.URL.Query()),
+	}
+
+	userMood, err := c.UserMoodService.GetUserMoodByUserID(userID, userMoodFilters)
+	if err != nil {
+		CheckError(err, w, r)
+		return
+	}
+
+	response := presenter.ToUserMoodPresenter(*userMood)
+	render.Status(r, http.StatusOK)
+	render.Render(w, r, &response)
+}
