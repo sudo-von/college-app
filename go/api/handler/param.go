40a3b3beeca56ba03fc6d3747e5306fb1c9f9add
VoN <sudo.von.contact@gmail.com>
2021-08-23 14:37:33 -0500

    feat: :sparkles: Add ParamToDate func to handle dates sent by url

--- go/api/handler/param.go
+++ go/api/handler/param.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/url"
 	"strconv"
+	"time"
 )
 
 func ParamToInt(param string, url url.Values) int {
@@ -15,4 +16,15 @@ func ParamToInt(param string, url url.Values) int {
 		return value
 	}
 	return value
+}
+
+func ParamToDate(param string, url url.Values) *time.Time {
+	if s := url.Get(param); s != "" {
+		value, err := time.ParseInLocation("2006-01-02", s, time.Local)
+		if err != nil {
+			return nil
+		}
+		return &value
+	}
+	return nil
 }
