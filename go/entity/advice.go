6f8bdfdff4a32207c442e693c241ee90fa9847c7
VoN <sudo.von.contact@gmail.com>
2021-08-24 00:09:24 -0500

    refactor: :recycle: Update advice entities structs

--- go/entity/advice.go
+++ go/entity/advice.go
@@ -4,29 +4,28 @@ import "time"
 
 type Advice struct {
 	ID           string
+	User         TinyUser
+	University   University
 	Subject      string
 	AdviceDate   time.Time
 	Classroom    int
-	User         TinyUser
-	University   University
 	Status       string
 	CreationDate time.Time
 }
 
 type AdvicePayload struct {
 	ID           string
+	UserID       string
+	UniversityID string
 	Subject      string
 	AdviceDate   time.Time
 	Classroom    int
-	UserID       string
-	UniversityID string
 	Status       string
 	CreationDate time.Time
 }
 
 type AdviceFilters struct {
-	InitialDate  *time.Time
-	FinalDate    *time.Time
 	UserID       string
 	UniversityID string
+	AdviceDate   *time.Time
 }
