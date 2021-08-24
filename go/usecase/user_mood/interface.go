bda2e867432a47f03c81426d448056358fa71206
VoN <sudo.von.contact@gmail.com>
2021-08-23 20:25:31 -0500

    feat: :sparkles: Add CreateUserMood method for the writer interface

--- go/usecase/user_mood/interface.go
+++ go/usecase/user_mood/interface.go
@@ -8,10 +8,16 @@ type Reader interface {
 	GetUserMoodByUserID(userID string, userMoodFilters entity.UserMoodFilters) (*entity.UserMood, error)
 }
 
+type Writer interface {
+	CreateUserMood(newUserMood entity.UserMoodPayload) error
+}
+
 type UserMoodRepository interface {
 	Reader
+	Writer
 }
 
 type UseCase interface {
 	GetUserMoodByUserID(userID string, userMoodFilters entity.UserMoodFilters) (*entity.UserMood, error)
+	CreateUserMood(newUserMood entity.UserMoodPayload) error
 }
