21df9f440597beb4aef832d6e46662487084a71d
VoN <sudo.von.contact@gmail.com>
2021-08-23 20:25:48 -0500

    feat: :sparkles: Add CreateUserMood method for the user mood service

--- go/usecase/user_mood/service.go
+++ go/usecase/user_mood/service.go
@@ -1,7 +1,9 @@
 package user_mood
 
 import (
+	"errors"
 	"freelancer/college-app/go/entity"
+	"time"
 )
 
 type Service struct {
@@ -23,4 +25,32 @@ func (s *Service) GetUserMoodByUserID(userID string, userMoodFilters entity.User
 
 	return userMood, nil
 
+}
+
+func (s *Service) CreateUserMood(newUserMood entity.UserMoodPayload) error {
+
+	// Checks if mood is a valid number in the specified range
+	validMood := false
+	if newUserMood.Mood >= 1 && newUserMood.Mood <= 5 {
+		validMood = true
+	}
+	if !validMood {
+		return errors.New("invalid mood, not in range from 1 to 5")
+	}
+	// Checks if the user mood has already been registered today.
+	currentDate := time.Now().In(time.Local)
+	userMoodFilters := entity.UserMoodFilters{
+		CreationDate: &currentDate,
+	}
+	_, err := s.GetUserMoodByUserID(newUserMood.UserID, userMoodFilters)
+	if err == nil {
+		return errors.New("user mood has already been registered today")
+	}
+
+	// Stores new user mood.
+	err = s.userMoodRepository.CreateUserMood(newUserMood)
+	if err != nil {
+		return err
+	}
+	return nil
 }
