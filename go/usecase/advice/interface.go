e19ddbf8a92c94718025e63f61635baeacc1646f
VoN <sudo.von.contact@gmail.com>
2021-08-25 00:56:23 -0500

    feat: :sparkles: Add writer interface and create advice method for the advice repository

--- go/usecase/advice/interface.go
+++ go/usecase/advice/interface.go
@@ -8,10 +8,16 @@ type Reader interface {
 	GetAdvices(universityID string, adviceFilters entity.AdviceFilters) ([]entity.Advice, *int, error)
 }
 
+type Writer interface {
+	CreateAdvice(newAdvice entity.AdvicePayload) error
+}
+
 type AdviceRepository interface {
 	Reader
+	Writer
 }
 
 type UseCase interface {
 	GetAdvices(userID string, adviceFilters entity.AdviceFilters) ([]entity.Advice, *int, error)
+	CreateAdvice(newAdvice entity.AdvicePayload) error
 }
