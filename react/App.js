23a4aa95062257ab36b958c09e94d577d0d5c978
VoN <sudo.von.contact@gmail.com>
2021-08-27 02:00:35 -0500

    feat: :heavy_plus_sign: Add react native paper

--- react/App.js
+++ react/App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Provider as PaperProvider } from 'react-native-paper'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <PaperProvider>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </PaperProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
