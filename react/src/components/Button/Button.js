7314da72d70377a264ee6f9f30636c9fa565ab97
VoN <sudo.von.contact@gmail.com>
2021-08-28 02:22:51 -0500

    feat: :sparkles: Add button component

---
+++ react/src/components/Button/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+/* React native paper. */
+import { Button } from 'react-native-paper'
+
+const CustomButton = ({...rest}) =>
+    <Button style={styles.button} mode='contained' {...rest}>
+        Iniciar sesión
+    </Button>
+
+const styles = StyleSheet.create({
+    button: {
+        marginTop: 35,
+        backgroundColor: '#4C9DAF',
+        color: 'white'
+    }
+})
+
+export default CustomButton
