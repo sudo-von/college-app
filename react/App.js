8d9824c9e11e7d9f9937554a04fafa970b8c3030
VoN <sudo.von.contact@gmail.com>
2021-08-28 02:26:44 -0500

    feat: :sparkles: Add AuthProvider to the Router

--- react/App.js
+++ react/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-/* React-native-paper. */
+/* React native paper. */
 import { Provider as PaperProvider } from 'react-native-paper'
 /* React router. */
-import { NativeRouter, Route, Link } from 'react-router-native'
+import { NativeRouter, Route } from 'react-router-native'
 /* Routes. */
 import Login from './src/pages/Login'
+/* Contexts. */
+import { AuthProvider } from './src/providers/auth.provider'
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NativeRouter>
-        <Route exact path="/" component={Login}/>
-      </NativeRouter>
-    </PaperProvider>
+    <AuthProvider>
+      <PaperProvider>
+        <NativeRouter>
+          <Route exact path="/" component={Login}/>
+        </NativeRouter>
+      </PaperProvider>
+    </AuthProvider>
   )
 }
