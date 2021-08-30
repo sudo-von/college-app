3f4b5007b7af12e34d2e6a265f408aac2ee98d3f
VoN <sudo.von.contact@gmail.com>
2021-08-29 21:33:49 -0500

    feat: :sparkles: Update login user service method

--- react/src/services/user.service.js
+++ react/src/services/user.service.js
@@ -1,5 +1,10 @@
 /* Base64. */
 import base64 from 'react-native-base64'
+/* Helpers. */
+import axios from 'src/helpers/axios-helper'
+import { setToken, decodeToken } from 'src/helpers/auth-helper'
+/* Constants. */
+import { LOGIN } from 'src/constants'
 
 export {
     login
@@ -7,40 +12,19 @@ export {
 
 /* Handles basic auth. */
 const login = async ({ email, password }) => {
-    
-    const options = {
-        method: 'POST',
-        headers: { 
-            'Authorization': "Basic " + base64.encode(`${email}:${password}`)
-        }
-    }
-
     try{
-        const response = await fetch(`http://192.168.0.4:4000/users/login`, options)
-        if(!response.ok){
-            throw new Error('Credenciales inválidas')
-        }
-        return response.headers.map.authorization
-    }catch(error){
-        return error
-    }
-}
-
-
-const handleResponse = (response) => {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                logout();
-                location.reload(true);
+        const response = await axios.post(LOGIN,{},{
+            "headers": { 
+                'Authorization': "Basic " + base64.encode(`${email}:${password}`)
             }
-
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
+        })
+        await setToken(response.headers.authorization)
+        const user = await decodeToken(response.headers.authorization)
+        return user
+    }catch(error){
+        if(error.response?.status == 401){
+            throw new Error("Credenciales inválidas")
         }
-
-        return data;
-    });
+        throw new Error("Ha ocurrido un error, intenta de nuevo más tarde")
+    }
 }
