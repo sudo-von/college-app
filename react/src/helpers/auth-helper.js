869c8e84ff40d368534f50b6815844e149d8692f
VoN <sudo.von.contact@gmail.com>
2021-08-29 21:33:25 -0500

    feat: :sparkles: Add decode token helper

--- react/src/helpers/auth-helper.js
+++ react/src/helpers/auth-helper.js
@@ -1,4 +1,7 @@
+/* Async storage. */
 import AsyncStorage from '@react-native-async-storage/async-storage'
+/* Base64. */
+import Base64 from 'Base64'
 
 const getToken = async () => {
     try{
@@ -17,9 +20,22 @@ const setToken = async (token) => {
     }
 }
 
-const deleteToken = () => {
+const deleteToken = async () => {
     try{
         await AsyncStorage.removeItem('token')
+    }catch(error){
+        return error
+    }
+}
+
+const decodeToken = async () => {
+    try{
+        const bearerToken = await getToken()
+        const encodedToken = bearerToken.split('Bearer ')[1]
+        const userToken = encodedToken.split('.')[1]
+        const user = Base64.atob(userToken)
+        const parsedUser = JSON.parse(user)
+        return parsedUser
     }catch(error){
         return error
     }
@@ -28,5 +44,6 @@ const deleteToken = () => {
 export { 
     getToken,
     setToken,
-    deleteToken
+    deleteToken,
+    decodeToken
 }
