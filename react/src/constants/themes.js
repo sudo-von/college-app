a9463f0430a23dcec3caeb88fdf54c0bd68d51d8
VoN <sudo.von.contact@gmail.com>
2021-09-01 18:49:02 -0500

    feat: :sparkles: Add theme  and endpoints constants,

---
+++ react/src/constants/themes.js
@@ -0,0 +1,16 @@
+/* React native paper. */
+import { DefaultTheme } from 'react-native-paper'
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#FFFFFF',
+    primary: '#4C9DAF',
+    placeholder: '#7D8286'
+   },
+}
+
+export { 
+    theme 
+}
