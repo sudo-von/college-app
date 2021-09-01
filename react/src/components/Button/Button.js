8dc970344ce4640d612e615be1c831d0393eacec
VoN <sudo.von.contact@gmail.com>
2021-09-01 18:54:00 -0500

    style: :lipstick: Button now accepts style prop

--- react/src/components/Button/Button.js
+++ react/src/components/Button/Button.js
@@ -3,16 +3,25 @@ import { StyleSheet } from 'react-native'
 /* React native paper. */
 import { Button } from 'react-native-paper'
 
-const CustomButton = ({loading, children, ...rest}) =>
-    <Button style={styles.button} mode='contained' loading={loading} disabled={loading} {...rest}>
-        {loading ? 'Cargando... ' : children}
+const CustomButton = ({loading, loadingMessage='Cargando...', children, ...rest}) =>
+    <Button 
+        style={styles.button} 
+        mode='contained' 
+        loading={loading} 
+        disabled={loading} 
+        labelStyle={styles.label}
+        {...rest}
+    >
+        {loading ? loadingMessage : children}
     </Button>
 
 const styles = StyleSheet.create({
+    label: {
+        color: 'white'
+    },
     button: {
         marginTop: 40,
         marginBottom: 40,
-        backgroundColor: '#4C9DAF',
         color: 'white'
     }
 })
