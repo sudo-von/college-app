0a530ca011ec4f329c2c72195ad83e34747a44f3
VoN <sudo.von.contact@gmail.com>
2021-08-31 23:34:22 -0500

    refactor: :recycle: Use react navigation instead of react router

--- react/App.js
+++ react/App.js
@@ -1,20 +1,29 @@
 import React from 'react'
 /* React native paper. */
 import { Provider as PaperProvider } from 'react-native-paper'
-/* React router. */
-import { NativeRouter, Route } from 'react-router-native'
+/* React navigation. */
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 /* Routes. */
 import Login from './src/pages/Login'
+import Signup from './src/pages/Signup'
 /* Contexts. */
 import { AuthProvider } from './src/providers/auth.provider'
+/* Refs. */
+import { navigationRef } from './src/refs/navigation.ref'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <AuthProvider>
       <PaperProvider>
-        <NativeRouter>
-          <Route exact path="/" component={Login}/>
-        </NativeRouter>
+        <NavigationContainer ref={navigationRef}>
+          <Stack.Navigator>
+            <Stack.Screen name="/" component={Login}/>
+            <Stack.Screen name="/signup" component={Signup}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </PaperProvider>
     </AuthProvider>
   )
