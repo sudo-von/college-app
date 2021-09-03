a23048fdce4636e476ca969db71c54b882b0b72f
VoN <sudo.von.contact@gmail.com>
2021-09-02 23:37:25 -0500

    feat: :sparkles: Add Logout component to the protected rutes

--- react/src/router/ProtectedRoutes.js
+++ react/src/router/ProtectedRoutes.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 /* Routes. */
 import Home from 'src/pages/Home'
+import Logout from 'src/pages/Logout'
 
 const Drawer = createDrawerNavigator()
 
@@ -12,6 +13,16 @@ const ProtectedRoutes = () =>
             name="/" 
             component={Home} 
             options={{ title: 'Inicio' }}
+        />
+        <Drawer.Screen 
+            name="/logout" 
+            component={Logout} 
+            options={
+                { 
+                    title: 'Cerrar sesión',
+                    header: () => null
+                }
+            }
         />
     </Drawer.Navigator>
 
