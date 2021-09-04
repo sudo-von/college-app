ecf9c54a37afcea5841a6a24d94b1f7dfc5c820a
VoN <sudo.von.contact@gmail.com>
2021-09-04 17:24:23 -0500

    refactor: :recycle: Make a components folder inside router's folder to handle its components

---
+++ react/src/router/Components/PublicStackNavigator/PublicStackNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react'
+/* React navigation. */
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+/* Routes. */
+import Login from 'src/pages/Login'
+import Signup from 'src/pages/Signup'
+
+const Stack = createNativeStackNavigator()
+
+const PublicStackNavigator = () =>
+    <Stack.Navigator>
+        <Stack.Screen 
+            name="/login" 
+            component={Login} 
+            options={{header: () => null}}
+        />
+        <Stack.Screen 
+            name="/signup" 
+            component={Signup}
+            options={{header: () => null}}
+        />
+    </Stack.Navigator>
+
+export default PublicStackNavigator
