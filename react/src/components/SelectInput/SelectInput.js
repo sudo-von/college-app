0e75b62bba0917eb144aa6a29a8d6ddaad9dd72c
VoN <sudo.von.contact@gmail.com>
2021-09-03 23:27:00 -0500

    feat: :sparkles: Add SelectInput component

---
+++ react/src/components/SelectInput/SelectInput.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+/* React native paper. */
+import DropDown from 'react-native-paper-dropdown'
+  
+const SelectInput = ({ label, data }) => {
+  
+  const [showDropDown, setShowDropDown] = useState(false)
+  const [selectedOption, setSelectedOption] = useState('')
+
+  return (
+    <View style={styles.containerStyle}>
+      <DropDown
+        label={label}
+        mode='outlined'
+        visible={showDropDown}
+        showDropDown={() => setShowDropDown(true)}
+        onDismiss={() => setShowDropDown(false)}
+        value={selectedOption}
+        setValue={setSelectedOption}
+        list={data}
+      />
+    </View>
+  )
+}
+  
+const styles = StyleSheet.create({
+  containerStyle: {
+    marginTop: 5,
+  },
+})
+  
+export default SelectInput
