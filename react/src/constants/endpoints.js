ca0bdd769cf41499aab74b807cbcb1c754457bce
VoN <sudo.von.contact@gmail.com>
2021-09-06 22:39:37 -0500

    feat: :sparkles: Add suggestion constant

--- react/src/constants/endpoints.js
+++ react/src/constants/endpoints.js
@@ -1,5 +1,6 @@
 /* Base. */
 export const API_URL = 'http://192.168.0.4:4000'
+export const SUGGESTION = `/suggestions`
 export const LOGIN = `/users/login`
 export const SIGNUP = `/users`
 export const MOOD = `/users-mood`
