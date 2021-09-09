b9ba5b0e8700e77cac2abba2ed4a5bcbf576534d
VoN <sudo.von.contact@gmail.com>
2021-09-09 01:01:05 -0500

    feat: :sparkles: Add user mood errors

---
+++ react/src/constants/errors.js
@@ -0,0 +1,11 @@
+export const USER_MOOD_ERRORS = {
+    "USER_MOOD_NOT_FOUND": {
+        "esp": "El estado de ánimo no ha sido encontrado."
+    },
+    "USER_MOOD_ALREADY_REGISTERED": {
+        "esp": "El estado de ánimo ya ha sido registrado."
+    },
+    "INVALID_MOOD_VALUE" : {
+        "esp" : "El estado de ánimo no se encuentra en el rango válido."
+    }
+}
