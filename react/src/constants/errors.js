cf87b9b22d2e45f520a4d51ade0535915b49bf4a
VoN <sudo.von.contact@gmail.com>
2021-09-09 01:06:16 -0500

    feat: :sparkles: Add user errors

--- react/src/constants/errors.js
+++ react/src/constants/errors.js
@@ -1,3 +1,39 @@
+export const USER_ERRORS = {
+    "UNAUTHORIZED_USER" : {
+        "esp": "El usuario no cuenta con los permisos necesarios para realizar esta acción."
+    },
+    "AUTH_HEADER_NOT_PROVIDED" : {
+        "esp": "El encabezado de autorización no fue proporcionado."
+    },
+    "INVALID_AUTH_HEADER_FORMAT" : {
+        "esp": "El formato del encabezado de autorización es inválido."
+    },
+    "INVALID_UNSUPPORTED_HEADER_FORMAT" : {
+        "esp": "El formato del encabezado de autorización no es soportado."
+    },
+    "INVALID_CREDENTIALS" : {
+        "esp": "Credenciales incorrectas."
+    },
+    "INVALID_TOKEN" : {
+        "esp": "El token es inválido o ha expirado."
+    },
+    "INVALID_USER_EMAIL" : {
+        "esp": "El correo proporcionado es inválido."
+    },
+    "INVALID_USER_REGISTRATION_NUMBER" : {
+        "esp": "La matrícula proporcionada es inválida."
+    },
+    "USER_NOT_FOUND" : {
+        "esp": "El usuario que estás buscando no fue encontrado."
+    },
+    "USER_EMAIL_ALREADY_REGISTERED" : {
+        "esp": "El correo que deseas utilizar ya se encuentra en uso."
+    },
+    "USER_REGISTRATION_NUMBER_ALREADY_REGISTERED" : {
+        "esp": "La matrícula que deseas utilizar ya se encuentra en uso."
+    }
+}
+
 export const USER_MOOD_ERRORS = {
     "USER_MOOD_NOT_FOUND": {
         "esp": "El estado de ánimo no ha sido encontrado."
