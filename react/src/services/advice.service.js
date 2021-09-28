2bb5c9c6f0a0b1b713ae8cf2ba49dda835e0fa7d
VoN <sudo.von.contact@gmail.com>
2021-09-27 22:50:19 -0500

    feat: :sparkles: Add delete advice by id and update students number functions for the advice service

--- react/src/services/advice.service.js
+++ react/src/services/advice.service.js
@@ -4,13 +4,31 @@ import { protectedAxios } from 'src/helpers/axios-helper'
 import { ADVICE } from 'src/constants/endpoints'
 
 export {
-    getAdvices
+    getAdvices,
+    deleteAdviceByID,
+    updateStudentsNumber
 }
 
 const getAdvices = async () => {
     try{
         const advices = await protectedAxios.get(`${ADVICE}`)
         return advices.data.results
+    }catch(error){
+        throw new Error(error.message)
+    }
+}
+
+const deleteAdviceByID = async (adviceID) => {
+    try{
+        await protectedAxios.delete(`${ADVICE}/${adviceID}`)
+    }catch(error){
+        throw new Error(error.message)
+    }
+}
+
+const updateStudentsNumber = async (adviceID) => {
+    try{
+        await protectedAxios.patch(`${ADVICE}/${adviceID}/students-number`, {})
     }catch(error){
         throw new Error(error.message)
     }
