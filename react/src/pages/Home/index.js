e2e2a14451798a47a6bde7e0118a5470bfcce851
VoN <sudo.von.contact@gmail.com>
2021-09-28 21:30:02 -0500

    perf: :zap: Remove unnecesary imports

--- react/src/pages/Home/index.js
+++ react/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { StyleSheet, View } from 'react-native'
 /* Custom components. */
 import Container from 'src/components/Container'
