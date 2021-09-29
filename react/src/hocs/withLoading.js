4cdacd5c2b258c8f24280881925a995dccf8b7b7
VoN <sudo.von.contact@gmail.com>
2021-09-28 21:25:24 -0500

    refactor: Update high order component

--- react/src/hocs/withLoading.js
+++ react/src/hocs/withLoading.js
@@ -1,10 +1,6 @@
 import React from 'react'
 /* Custom components. */
-import Container from 'src/components/Container'
-import Center from 'src/components/Center'
-import Small from 'src/components/Small'
-/* React native paper. */
-import { ActivityIndicator } from 'react-native-paper'
+import Loader from 'src/components/Loader'
 
 const withLoading = (Component, loadingMessage) => {
 
@@ -12,12 +8,7 @@ const withLoading = (Component, loadingMessage) => {
 
         if (isLoading){
             return (
-                <Container>
-                    <ActivityIndicator size={80}/>
-                    <Center>
-                        <Small>{loadingMessage}</Small>
-                    </Center>
-                </Container>
+                <Loader loadingMessage={loadingMessage}/>
             )
         }
         return <Component {...props}/>
