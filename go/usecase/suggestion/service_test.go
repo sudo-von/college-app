862f6a8d70983f5d2306cb75622adc4a7cd73a33
VoN <sudo.von.contact@gmail.com>
2021-10-06 21:47:39 -0500

    feat: :white_check_mark: Add tests

---
+++ go/usecase/suggestion/service_test.go
@@ -0,0 +1,43 @@
+package suggestion
+
+import (
+	"freelancer/college-app/go/entity"
+	"testing"
+	"time"
+)
+
+func TestService_CreateSuggestion(t *testing.T) {
+	type fields struct {
+		suggestionRepository SuggestionRepositoryMock
+	}
+	type args struct {
+		newSuggestion entity.SuggestionPayload
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				newSuggestion: entity.SuggestionPayload{
+					UserID:       "615c09f7309d7ded48c7a053",
+					Suggestion:   "Fake suggestion",
+					CreationDate: time.Date(2021, 01, 01, 0, 0, 0, 0, time.Local),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				suggestionRepository: tt.fields.suggestionRepository,
+			}
+			if err := s.CreateSuggestion(tt.args.newSuggestion); (err != nil) != tt.wantErr {
+				t.Errorf("Service.CreateSuggestion() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
