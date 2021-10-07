995b508e5f884fb6798badcd229e22047e8dadca
VoN <sudo.von.contact@gmail.com>
2021-10-06 20:06:05 -0500

    feat: :white_check_mark: Add user service tests

--- go/usecase/user/service_test.go
+++ go/usecase/user/service_test.go
@@ -72,3 +72,252 @@ func TestService_GetTinyUserByID(t *testing.T) {
 		})
 	}
 }
+
+func TestService_CreateUser(t *testing.T) {
+	type fields struct {
+		userRepository       UserRepositoryMock
+		universityRepository university.UniversityRepositoryMock
+	}
+	type args struct {
+		newUser entity.UserPayload
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				newUser: entity.UserPayload{
+					Name:               "Von",
+					BirthDate:          time.Date(1997, 04, 17, 0, 0, 0, 0, time.Local),
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "16190770",
+					Password:           "123456",
+					UniversityID:       "615c09f7309d7ded48c7a049",
+					Status:             entity.ActiveStatus,
+					Role:               entity.StudentRole,
+					CreationDate:       time.Date(2021, 01, 01, 0, 0, 0, 0, time.Local),
+				},
+			},
+		},
+		{
+			name: "invalid email",
+			args: args{
+				newUser: entity.UserPayload{
+					Email: "sudo.von.contact",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "registration number accepts only numbers",
+			args: args{
+				newUser: entity.UserPayload{
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "abcdefgh",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "exceeds the length of the registration number ",
+			args: args{
+				newUser: entity.UserPayload{
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "161907707",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "email already in use",
+			args: args{
+				newUser: entity.UserPayload{
+					Email:              "sudo.von.contact@gmail.com",
+					RegistrationNumber: "16190770",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "registration number already in use",
+			args: args{
+				newUser: entity.UserPayload{
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "16190775",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "university id not found",
+			args: args{
+				newUser: entity.UserPayload{
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "16190770",
+					UniversityID:       "615c09f7309d7ded48c7a048",
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				userRepository:       tt.fields.userRepository,
+				universityRepository: tt.fields.universityRepository,
+			}
+			if err := s.CreateUser(tt.args.newUser); (err != nil) != tt.wantErr {
+				t.Errorf("Service.CreateUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestService_UpdateTinyUser(t *testing.T) {
+	type fields struct {
+		userRepository       UserRepositoryMock
+		universityRepository university.UniversityRepositoryMock
+	}
+	type args struct {
+		userID          string
+		requestedUserID string
+		newUser         entity.UpdateUserPayload
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				userID:          "615c09f7309d7ded48c7a053",
+				requestedUserID: "615c09f7309d7ded48c7a053",
+				newUser: entity.UpdateUserPayload{
+					Name:               "Von",
+					BirthDate:          time.Date(1997, 04, 17, 0, 0, 0, 0, time.Local),
+					Email:              "sudo.von.contact@gmail.com",
+					RegistrationNumber: "16190770",
+				},
+			},
+		},
+		{
+			name: "invalid email",
+			args: args{
+				userID:          "615c09f7309d7ded48c7a053",
+				requestedUserID: "615c09f7309d7ded48c7a053",
+				newUser: entity.UpdateUserPayload{
+					Email: "sudo.von.contact",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "registration number accepts only numbers",
+			args: args{
+				userID:          "615c09f7309d7ded48c7a053",
+				requestedUserID: "615c09f7309d7ded48c7a053",
+				newUser: entity.UpdateUserPayload{
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "abcdefgh",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "exceeds the length of the registration number ",
+			args: args{
+				userID:          "615c09f7309d7ded48c7a053",
+				requestedUserID: "615c09f7309d7ded48c7a053",
+				newUser: entity.UpdateUserPayload{
+					Email:              "sudovoncontact@gmail.com",
+					RegistrationNumber: "161907707",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "email already in use",
+			args: args{
+				userID:          "615c09f7309d7ded48c7a053",
+				requestedUserID: "615c09f7309d7ded48c7a053",
+				newUser: entity.UpdateUserPayload{
+					Email:              "sudovonctf@gmail.com",
+					RegistrationNumber: "16190770",
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "registration number already in use",
+			args: args{
+				userID:          "615c09f7309d7ded48c7a053",
+				requestedUserID: "615c09f7309d7ded48c7a053",
+				newUser: entity.UpdateUserPayload{
+					Email:              "sudo.von.contact@gmail.com",
+					RegistrationNumber: "16190776",
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				userRepository:       tt.fields.userRepository,
+				universityRepository: tt.fields.universityRepository,
+			}
+			if err := s.UpdateTinyUser(tt.args.userID, tt.args.requestedUserID, tt.args.newUser); (err != nil) != tt.wantErr {
+				t.Errorf("Service.UpdateTinyUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestService_AuthenticateUser(t *testing.T) {
+	type fields struct {
+		userRepository       UserRepositoryMock
+		universityRepository university.UniversityRepositoryMock
+	}
+	type args struct {
+		email    string
+		password string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *entity.User
+		wantErr bool
+	}{
+		{
+			name: "invalid credentials",
+			args: args{
+				email:    "sudo.von.contact@gmail.com",
+				password: "123abc",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				userRepository:       tt.fields.userRepository,
+				universityRepository: tt.fields.universityRepository,
+			}
+			got, err := s.AuthenticateUser(tt.args.email, tt.args.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Service.AuthenticateUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Service.AuthenticateUser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
