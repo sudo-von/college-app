34e99d5a601579b294cd8f402461ac5e484653db
VoN <sudo.von.contact@gmail.com>
2021-10-12 03:34:05 -0500

    refactor: :recycle: Refactor styles.

--- react/src/components/Bold/Bold.js
+++ react/src/components/Bold/Bold.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { Text } from 'react-native'
+import { styles } from './Bold.styles'
 
 const Bold = ({ children, ...rest }) =>
-        <Text 
-            style={styles.text}
-            {...rest}
-        >
-            {children}
-        </Text>
-
-const styles = StyleSheet.create({
-    text: {
-        fontWeight: 'bold',
-    }
-})
+    <Text style={styles.text} {...rest}>
+        {children}
+    </Text>
 
 export default Bold
